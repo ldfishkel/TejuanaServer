1a543c3178e0fbb350a31d3b1143fe5efa0b00af
Leonel Dan <ldfishkel@gmail.com>
2016-11-30 03:19:28 -0300

    script.sql changes

--- script.sql
+++ script.sql
@@ -47,6 +47,7 @@ CREATE TABLE IF NOT EXISTS Tejuana.Product (
 	product_cost NUMERIC(15,2) NOT NULL,
 	product_size VARCHAR(10),
 	product_type_id INT,
+	product_stock INT NOT NULL,
 	product_tejuar_avg INT NOT NULL,
 	product_image VARCHAR(255) NOT NULL,
 	PRIMARY KEY (product_id),
@@ -81,7 +82,8 @@ AS
 		p.product_size,
 		s.supply_id,
 		s.supply_name,
-		ps.quantity
+		ps.quantity,
+		p.product_stock
 	FROM
 		Tejuana.Product AS p,
 		Tejuana.Product_Type AS pt,
@@ -131,20 +133,41 @@ CREATE PROCEDURE Tejuana.ProductListBy (prodType INT)
 
 DROP PROCEDURE IF EXISTS Tejuana.InsertProduct;
 DELIMITER //
-CREATE PROCEDURE Tejuana.InsertProduct (prodName VARCHAR(100), price NUMERIC(15,2), size VARCHAR(10), typeId INT, avgTime INT, imageUrl VARCHAR(255))
+CREATE PROCEDURE Tejuana.InsertProduct (prodName VARCHAR(100), price NUMERIC(15,2), size VARCHAR(10), typeId INT, avgTime INT, imageUrl VARCHAR(255), stock INT)
 BEGIN
+	DECLARE lastId INT;
+	
 	INSERT INTO 
-		Tejuana.Product (product_name, product_cost, product_size, product_type_id, product_tejuar_avg, product_image) 
+		Tejuana.Product (product_name, product_cost, product_size, product_type_id, product_tejuar_avg, product_image, product_stock) 
 	VALUES
-		(prodName, price, size, typeId, avgTime, imageUrl);
+		(prodName, price, size, typeId, avgTime, imageUrl, product_stock);
+	
+	SELECT last_insert_id();
+END //
+DELIMITER ;
 
-	SELECT LAST_INSERT_ID();
+DROP PROCEDURE IF EXISTS Tejuana.UpdateProduct;
+DELIMITER //
+CREATE PROCEDURE Tejuana.UpdateProduct (prodId INT, prodName VARCHAR(100), price NUMERIC(15,2), size VARCHAR(10), typeId INT, avgTime INT, imageUrl VARCHAR(255), stock INT)
+BEGIN
+	UPDATE 
+		Tejuana.Product
+	SET
+		product_name = prodName,
+		product_cost = price,
+		product_size = size,
+		product_type_id = typeId,
+		product_tejuar_avg = avgTime,
+		product_image = imageUrl,
+		product_stock = stock
+	WHERE
+		product_id = prodId;
 END //
 DELIMITER ;
 
 INSERT INTO Tejuana.Product_Type VALUES (1, 'Leodan');
-INSERT INTO Tejuana.Product VALUES (1, 'Leodan deluxe', 300, 'XL', 1, 120, 'https://scontent-gru2-1.xx.fbcdn.net/t45.5328-0/p180x540/14057493_1212721252133415_1344293813_n.jpg');
-INSERT INTO Tejuana.Product VALUES (2, 'Leodan platinum', 350, 'XL', 1, 120, 'https://fb-s-d-a.akamaihd.net/h-ak-xpt1/v/t1.0-9/15178202_1358275414192090_2732619573659142335_n.jpg?oh=9185a9027c5e552a90741ab3d7aea728&oe=58FC35F1&__gda__=1490022246_44d183b51eb2d5bbff97ab7da99ef6d5');
+INSERT INTO Tejuana.Product VALUES (1, 'Leodan deluxe', 300, 'XL', 1, 5, 120, 'https://scontent-gru2-1.xx.fbcdn.net/t45.5328-0/p180x540/14057493_1212721252133415_1344293813_n.jpg');
+INSERT INTO Tejuana.Product VALUES (2, 'Leodan platinum', 350, 'XL', 1, 6, 120, 'https://fb-s-d-a.akamaihd.net/h-ak-xpt1/v/t1.0-9/15178202_1358275414192090_2732619573659142335_n.jpg?oh=9185a9027c5e552a90741ab3d7aea728&oe=58FC35F1&__gda__=1490022246_44d183b51eb2d5bbff97ab7da99ef6d5');
 INSERT INTO Tejuana.Supply VALUES (1, 'Hilo Rojo');
 INSERT INTO Tejuana.Supply VALUES (2, 'Hilo Crudo');
 INSERT INTO Tejuana.Supply VALUES (3, 'Hilo Amarillo');
@@ -154,13 +177,8 @@ INSERT INTO Tejuana.Product_Supply VALUES (2, 3, 50);
 INSERT INTO Tejuana.Product_Supply VALUES (2, 2, 150);
 
 INSERT INTO Tejuana.Product_Type VALUES (2, 'Top Mery');
-INSERT INTO Tejuana.Product VALUES (3, 'Top Mery Rosa', 200, 'S', 2, 100, 'https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-0/p480x480/15219369_1352039674815664_2465993764460513865_n.jpg?oh=416acbb69780bd6cd6136b26bf6c0ee3&oe=58BDB101');
-INSERT INTO Tejuana.Product VALUES (4, 'Top Mery Crudo', 220, 'M', 2, 100, 'https://fb-s-c-a.akamaihd.net/h-ak-xpt1/v/t1.0-9/15094480_1343335192352779_4701827482473836564_n.jpg?oh=bb52701e6900cc4555ca72bc96d6ee24&oe=58BA51D4&__gda__=1488906051_5454fcbb81a6f36764bdc17204ab98b6');
+INSERT INTO Tejuana.Product VALUES (3, 'Top Mery Rosa', 200, 'S', 2, 7, 100, 'https://scontent-gru2-1.xx.fbcdn.net/v/t1.0-0/p480x480/15219369_1352039674815664_2465993764460513865_n.jpg?oh=416acbb69780bd6cd6136b26bf6c0ee3&oe=58BDB101');
+INSERT INTO Tejuana.Product VALUES (4, 'Top Mery Crudo', 220, 'M', 2, 8, 100, 'https://fb-s-c-a.akamaihd.net/h-ak-xpt1/v/t1.0-9/15094480_1343335192352779_4701827482473836564_n.jpg?oh=bb52701e6900cc4555ca72bc96d6ee24&oe=58BA51D4&__gda__=1488906051_5454fcbb81a6f36764bdc17204ab98b6');
 INSERT INTO Tejuana.Supply VALUES (4, 'Hilo Rosa');
 INSERT INTO Tejuana.Product_Supply VALUES (3, 4, 200);
 INSERT INTO Tejuana.Product_Supply VALUES (4, 2, 250);
-
-
-
-
-
